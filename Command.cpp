5c7c3fac58c1d30d3389c7655bd858218d5b92e1
Fantasy Blue <polaris0x00@163.com>
2025-12-14 15:20:49 +0800

    模块化 Command.hpp

---
+++ Command.cpp
@@ -0,0 +1,183 @@
+#include "Command.hpp"
+#include "ReadStorage.hpp"
+#include <Arduino.h>
+#include <Wire.h>
+
+#define DEBUG 0
+
+#if DEBUG
+#define DEBUG_PRINT(...) Serial.print(__VA_ARGS__)
+#define DEBUG_PRINTLN(...) Serial.println(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#define DEBUG_PRINTLN(...)
+#endif
+
+extern const int dataPin;
+extern const int clkPin;
+extern const int latchPin;
+extern const int enablePin;
+extern const int deviceAddress;
+
+extern const byte keyList[];
+
+const int keyListSize = 45;
+
+struct Command {
+  const char* name;
+  void (*func)(String);
+};
+
+struct Option {
+  const char* name;
+  int* option;
+};
+
+Command commands[] = {
+  { "ISUP", cmdIsUp },
+  { "DIAG", cmdDiag },
+  { "KEY", cmdKey },
+  { "SET", cmdSet },
+  { "GET", cmdGet },
+  { "RUN", cmdRun }
+};
+
+Option options[] = {
+  { "KEYTIME", &keyTime },
+  { "KEYDELAY", &keyDelay }
+};
+
+const int commandCount = sizeof(commands) / sizeof(Command);
+const int optionCount = sizeof(options) / sizeof(Option);
+
+extern void keyStroke(byte keyValue);
+
+void processCommand(String fullCommand) {
+  fullCommand.trim();
+  Serial.print("> ");
+  Serial.println(fullCommand);
+
+  int spaceIndex = fullCommand.indexOf(' ');
+  String cmdName = fullCommand;
+  String params = "";
+
+  if (spaceIndex > 0) {
+    cmdName = fullCommand.substring(0, spaceIndex);
+    params = fullCommand.substring(spaceIndex + 1);
+    params.trim();
+  }
+
+  cmdName.toUpperCase();
+
+  // 查找并执行命令
+  bool found = false;
+  for (int i = 0; i < commandCount; i++) {
+    if (cmdName.equals(commands[i].name)) {
+      commands[i].func(params);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    Serial.println("Invalid command");
+  }
+}
+
+void cmdIsUp(String params) {
+  Serial.println("OK");
+}
+
+void cmdDiag(String params) {
+  Serial.print("Running diagnostic...");
+  for (int i = 0; i < keyListSize; i++) {
+    keyStroke(keyList[i]);
+  }
+  Serial.println("OK");
+}
+
+void cmdKey(String params) {
+  unsigned int keyValue;
+  int count = sscanf(params.c_str(), "%x", &keyValue);
+
+  bool valid = false;
+  if (count == 1) {
+    for (int i = 0; i < keyListSize; i++) {
+      if (keyValue == keyList[i])
+        valid = true;
+    }
+  }
+
+  if (valid) {
+    keyStroke(keyValue);
+    Serial.println("OK");
+  } else {
+    Serial.println("Invalid key value");
+    return;
+  }
+}
+
+void cmdSet(String params) {
+  int spaceIndex = params.indexOf(' ');
+
+  if (spaceIndex <= 0) {
+    Serial.println("No enough arguments");
+    return;
+  }
+
+  String opt = params.substring(0, spaceIndex);
+  String value = params.substring(spaceIndex + 1);
+
+  int valueSet = value.toInt();
+
+  opt.toUpperCase();
+  bool found = false;
+
+  for (int i = 0; i < optionCount; i++) {
+    if (opt.equals(options[i].name)) {
+      *options[i].option = valueSet;
+      DEBUG_PRINTLN(value);
+      DEBUG_PRINTLN(valueSet);
+      DEBUG_PRINT(options[i].name);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    Serial.println("Invalid option");
+  }
+}
+
+void cmdGet(String params) {
+  params.toUpperCase();
+
+  for (int i = 0; i < optionCount; i++) {
+    if (params.equals(options[i].name)) {
+      Serial.println(*options[i].option);
+      break;
+    }
+  }
+}
+
+void cmdRun(String params) {
+  unsigned int beginAddr, endAddr;
+  int count = sscanf(params.c_str(), "%x %x", &beginAddr, &endAddr);
+  DEBUG_PRINTLN(count);
+  DEBUG_PRINTLN(beginAddr, HEX);
+  DEBUG_PRINTLN(endAddr, HEX);
+  if (count != 2 || beginAddr > endAddr) {
+    Serial.println("Invalid address");
+    return;
+  }
+
+  for (int addr = beginAddr; addr <= endAddr; addr++){
+    DEBUG_PRINT(addr,HEX);
+    DEBUG_PRINT(":");
+    DEBUG_PRINTLN(readROM(deviceAddress, addr), HEX);
+    keyStroke(readROM(deviceAddress, addr));
+    Serial.print(((float)(addr - beginAddr) / (float)(endAddr - beginAddr))*100, 2);
+    Serial.println("%");
+  }
+  Serial.println("OK");
+}
