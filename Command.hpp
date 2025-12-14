cb5b76ad393baed42799114df1bb16ac65e0778d
Fantasy Blue <polaris0x00@163.com>
2025-12-14 18:50:12 +0800

    实现check命令 修改模块结构

--- Command.hpp
+++ Command.hpp
@@ -3,10 +3,16 @@
 
 #include <Arduino.h>
 
+extern const int dataPin;
+extern const int clkPin;
+extern const int latchPin;
+extern const int enablePin;
+extern const int deviceAddress;
+
 extern int keyTime;
 extern int keyDelay;
 extern const byte keyList[];
-extern const int deviceAddress;
+extern const int keyListSize;
 
 void processCommand(String fullCommand);
 void cmdIsUp(String params);
@@ -15,5 +21,6 @@ void cmdKey(String params);
 void cmdSet(String params);
 void cmdGet(String params);
 void cmdRun(String params);
+void cmdCheck(String params);
 
 #endif
